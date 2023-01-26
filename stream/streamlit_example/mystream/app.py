e5597f5dfad9e50bd2bbad53269cf9f88b845b99
seok9924 <41310927+seok9924@users.noreply.github.com>
2023-01-26 16:11:41 +0900

    asd
    asd

--- stream/streamlit_example/mystream/app.py
+++ stream/streamlit_example/mystream/app.py
@@ -1,50 +1,102 @@
 import streamlit as st
+import datetime
+import time
+import pandas as pd
+import lxml
+import streamlit as st
+import pandas as pd
+import yfinance as yf
+import datetime
+import matplotlib.pyplot as plt
+import matplotlib 
+from io import BytesIO
+
+def conver_df(df):
+    return df.to_csv().encode('utf-8')
+
 
 # 이미지를 첨부하여 업로드하려면...
-from PIL import Image # 파이썬 기본라이브러리는 바로 사용 가능!
 import os
 abs_path = os.path.dirname(os.path.abspath(__file__)) 
 import streamlit.components.v1 as components
 
+def get_stock_info(maket_type=None):
+    base_url =  "http://kind.krx.co.kr/corpgeneral/corpList.do"    
+    method = "download"
+    if maket_type == 'kospi':         
+        marketType = "stockMkt"      
+    elif maket_type == 'kosdaq':
+        marketType = "kosdaqMkt"    
+    elif maket_type == None:         
+        marketType = ""
+    url = "{0}?method={1}&marketType={2}".format(base_url, method, marketType)     
+    df = pd.read_html(url, header=0)[0]
+    df['종목코드']= df['종목코드'].apply(lambda x: f"{x:06d}")     
+    df = df[['회사명','종목코드']]
+    return df
+
+def get_ticker_symbol(company_name, maket_type):     
+    df = get_stock_info(maket_type)
+    code = df[df['회사명']==company_name]['종목코드'].values    
+    code = code[0]
+    if maket_type == 'kospi':   
+        ticker_symbol = code +".KS"     
+    elif maket_type == 'kosdaq':      
+        ticker_symbol = code +".KQ" 
+    return ticker_symbol,code
+
+
 
 with st.sidebar:
-    add_radio = st.radio(
-        "Choose a shipping method",
-        ("Standard (5-15 days)", "Express (2-5 days)")
+    st.write("회사 이름과 입력 기간")
+    company = st.text_input('주식회사명', '삼성전자')
+    
+    market_choice = st.radio(
+        "주식 시장을 고르세요",
+        ("kospi", "kosdaq")
+        
     )
+    market_code_button=st.button('시장코드찾기')
+    if (market_code_button==True):
+        stock_info=get_stock_info(maket_type=market_choice)
+        st.write(stock_info)
+        
+    
+    
+    date_se=st.sidebar.date_input('시작일과 종료일',
+                 [datetime.date(2022, 1, 1), datetime.date(2023, 1, 1)]) 
+    search=st.button('검색')
 
-st.sidebar.write('사이드바입니다')
 
-st.sidebar.image('https://imgur.com/a/vW5V5OV')
+st.title("주식정보를 가져오는 웹앱")
 
-[col1, col2, col3] = st.columns(3) 
 
-with col1:
-    st.header('1. IMAGE')
-    st.image("https://i.imgur.com/10J2i4y.jpg")
-with col2:
-    st.header('2. VIDEO')
-    # 저작권 무료 음악: https://studio.youtube.com/channel/UCjMXxVpbAqejiHiMh9nlPIg/music
-    # st.audio('.\Emotional Mess - Amy Lynn & the Honey Men.mp3')
-    st.video('https://www.youtube.com/watch?v=60ItHLz5WEA', format="video/mp4", start_time=0)
+       
 
-with col3:
-    st.header('3. HTML')
+checkmarket=False
+abc=get_stock_info(market_choice)
 
-    html = """
-    <div style='
-        background-color:red;
-        color:white;
-    '>
-        HTML 코드로 작성했습니다 근데 아래의 style태그 때문에  가장 위의 # 필수항목만 넣은 페이지 까지 빨갛게 변한 것 확인되시나요
-        그래서 Streamlit은 기본적으로는 자체 메소드들로만 화면을 구성할 것을 권장합니다.
-    </div>
 
-    <style>
-    h1 {
-        color : red;
-    }
-    <style>
-    """
+           
 
-    st.markdown(html, unsafe_allow_html=True)
+
+if(search==True) :
+    st.write(company+'의 회사코드입니다')
+    serial=get_ticker_symbol(company,market_choice)
+    st.write(serial[0])
+    st.write(date_se[0])
+    date_range= date_se   
+    ticker_symbol = serial     
+    ticker_data = yf.Ticker(ticker_symbol)
+    start_p = date_range[0]               
+    end_p = date_range[1] + datetime.timedelta(days=1) 
+    df = ticker_data.history(start=start_p, end=end_p)
+    df.index = df.index.date
+    st.subheader(f"[{company}] 주가 데이터")
+    st.dataframe(df.head(10))
+    st.line_chart(df)
+    st.download_button('CSV 파일로 다운받기',conver_df(df), file_name=company+date_se[0].strftime('%Y/%m/%d')+'~'+date_se[1].strftime('%Y/%m/%d')+'.csv',mime='text/csv')
+    
+    
+    
+
